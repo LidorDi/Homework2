b9487fe8102b22ace6baa46d02f6b495de3cc4ac
LidorDi <Lidorcrypxd@gmail.com>
2025-02-15 19:11:38 +0200

    Add files via upload

---
+++ Sql 12.01.py
@@ -0,0 +1,19 @@
+#1
+
+# partition makes the rows go through a simillar command as group by (it sorts them by employee id)
+
+#2
+
+# the command makes a new table with 2 values one of the values which is "details" can contain multiple values
+# in the end the command calls the values in details (not all of them) and names them showing the name of the product and how much ram it has
+
+#3
+
+# makes a new table adding id and array of numbers using the [], first command will call the first element (first value) in each array with its own id
+# second one will call only the row where the number 1 is in if there is one
+
+#4
+
+# The command makes the tax column more like automated one. it read only column which adds values according to the formula u have specified afther the GENERATED ALWAYS AS command
+
+# pythonai250824+HWSQL5@gmail.com
